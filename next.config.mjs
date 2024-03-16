dbff4a22a2de84fbbc94b6dfef60440c087c735b
ooexiaoo <vpaherwar@gmail.com>
2024-03-17 01:53:59 +0530

    build test on "public1" folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'public1'
+};
 
 export default nextConfig;
---
+++ public1/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"9ckl0WznIoiI/BQ0TvemjgSmv8ATJrKJLg9ezJ8hm/o=\"}"
