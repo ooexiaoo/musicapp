f4b0bbf31b1c9fe58b4aca8310cbc5e4d551b553
ooexiaoo <vpaherwar@gmail.com>
2024-03-18 16:59:44 +0530

    Meet our instructors

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: 'public1'
+    distDir: 'public1',
+    images: {
+        domains: ['images.unsplash.com', 'red.cloudinary.com']
+    }
 };
 
 export default nextConfig;
---
+++ public1/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SWpSgaDq0VpVsavvtSwVop7jCEt1igXVMGL9R04a5ZU=\"\n}"
