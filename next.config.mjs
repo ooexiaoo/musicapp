406f2f02ea9f1afbbe98ad93af9a0d9564261e76
ooexiaoo <vpaherwar@gmail.com>
2024-03-19 20:19:32 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: "/nextjs-github-pages",
     reactStrictMode: true,
     distDir: 'public1',
     images: {
+        unoptimized: true,
         domains: ['images.unsplash.com', 'red.cloudinary.com']
     }
 };
